4e977d7f6c0d206f40ebab454b975d95062b37a6
von Buelow <jaspar@UMICH.EDU>
2020-08-10 21:15:30 -0400

    actionTypes - remove post

--- src/rootReducer.js
+++ src/rootReducer.js
@@ -4,11 +4,11 @@ const INITIAL_STATE = {posts: {"1294u0239-12423523": {
     title: "This is a Blog Title",
     description: "This is the subtitle",
     body: "Hello world lskjrosiej lrskjelr",
-    comments: [
-      {id: "2342340-23dslkj3lkj", text:"This is a cool blog"},
-      {id: "2342340-23dslkj3l323", text:"Messi is the GOAT"},
-      {id: "2342340-23dslkj3l53561", text:"Awesome post"},
-    ]
+    comments: {
+      "2342340-23dslkj3lkj": {text: "This is a cool blog"},
+      "2342340-23dslkj3l323": {text: "Messi is the GOAT"},
+      "2342340-23dslkj3l53561": {text: "Awesome post"}
+    }
   }
 }};
 
@@ -17,6 +17,11 @@ function rootReducer(state = INITIAL_STATE, action) {
     case ADD_POST: {
       const postsCopy = { ...state.posts };
       postsCopy[action.id] = action.post;
+      return { ...state, posts: postsCopy };
+    }
+    case REMOVE_POST: {
+      const postsCopy = { ...state.posts };
+      delete postsCopy[action.id];
       return { ...state, posts: postsCopy };
     }
       
