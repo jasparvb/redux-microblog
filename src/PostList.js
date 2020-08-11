c14da3a9779ff4c8ff173c2fbb8085f1b25d66ec
von Buelow <jaspar@UMICH.EDU>
2020-08-10 21:50:02 -0400

    Rewrite PostList with redux

--- src/PostList.js
+++ src/PostList.js
@@ -1,25 +1,29 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 
-function PostList({posts}) {
-    if (!posts.length) return <h1>You don't have any posts yet</h1>;
-  
+function PostList() {
+    const posts = useSelector(state => state.posts);
+    if (!Object.keys(posts).length) return <h1>You don't have any posts yet</h1>;
+    
+    const postCards = Object.keys(posts).map(id => (
+            <div key={id} className="Card card my-2" >
+            <div className="card-body">
+                <Link to={`/${id}`}>
+                    <h3 className="card-title d-flex justify-content-between">
+                        {posts[id].title}
+                    </h3>
+                </Link>            
+                <p>{posts[id].description}</p>
+            </div>
+        </div>
+    ));
+
     return (
         <div className="PostList">
             <div className="container text-left">
-                {posts.map(p => 
-                    <div key={p.id} className="Card card my-2" >
-                        <div className="card-body">
-                            <Link to={`/${p.id}`}>
-                                <h3 className="card-title d-flex justify-content-between">
-                                    {p.title}
-                                </h3>
-                            </Link>            
-                            <p>{p.description}</p>
-                        </div>
-                    </div>
-                )}
+                {postCards}
             </div>
         </div>
     );
