807dffb50e321e4cc24a163cd90b8846bddcdfe6
von Buelow <jaspar@UMICH.EDU>
2020-08-10 21:29:51 -0400

    Post and comment actions

---
+++ src/actions.js
@@ -0,0 +1,33 @@
+import { ADD_POST, REMOVE_POST, ADD_COMMENT, REMOVE_COMMENT } from "./actionTypes";
+
+export function addPost(id, post) {
+    return {
+        type: ADD_POST,
+        id,
+        post
+    };
+}
+
+export function removePost(id) {
+    return {
+        type: REMOVE_POST,
+        id
+    };
+}
+
+export function addComment(post_id, id, comment) {
+    return {
+        type: ADD_COMMENT,
+        post_id,
+        id,
+        comment
+    };
+}
+
+export function removeComment(post_id, id) {
+    return {
+        type: REMOVE_COMMENT,
+        post_id,
+        id
+    };
+}
